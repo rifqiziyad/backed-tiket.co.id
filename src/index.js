e30b5ae4a07388442cb9834f32b25f337dd76627
rifqiziyad <rifqiimtinan@gmail.com>
2021-07-22 21:31:24 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const routerNavigation = require('./routes/')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.DB_PORT
+const port = process.env.PORT
 
 app.use(morgan('dev'))
 app.use(cors())
